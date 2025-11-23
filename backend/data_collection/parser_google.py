6c4fb08d53097902b93fbb6e34d3f7be7d2bb29d
Rui Chang <124597595+RChang06@users.noreply.github.com>
2025-11-22 23:32:56 -0800

    Update parser_google.py

--- backend/data_collection/parser_google.py
+++ backend/data_collection/parser_google.py
@@ -11,12 +11,53 @@ def main():
         print(f)
 
     print("\nYouTube:")
-    for s in client.youtube.list_subscriptions():
-        print(s)
+
+    print("New subscriptions within a certain date")
+    subs = client.youtube.list_subscriptions_in_range(
+        "2023-03-15T00:00:00Z",  # Use past dates
+        "2025-11-15T23:59:59Z",
+        limit=30
+    )
+    print(subs)
+
+
+    print("Fetching liked videos")
+    liked_data = client.youtube.get_user_liked_videos_in_range(
+        "2023-03-15T00:00:00Z",
+        "2025-11-15T23:59:59Z",
+        limit=25
+    )
+    if liked_data.get("count", 0) > 0:
+        print(f"\nFound {liked_data['count']} liked videos:")
+
+        for video in liked_data["videos"]:
+            print(f"  - Title: {video['title']}")
+            print(f"    Video ID: {video['video_id']}")
+            print(f"    Liked At: {video['liked_at']}\n")
+    else:
+        print("No liked videos found in that time period.")
+
+   
+   
+    top_creators = client.youtube.get_most_liked_creators(
+    start_date="2023-03-15T00:00:00Z",
+    end_date="2025-11-15T23:59:59Z"
+    )
+
+    # Print the results
+    if top_creators:
+        for i, creator in enumerate(top_creators, 1):
+            print(f"{i}. {creator['name']} - {creator['liked_count']} liked videos")
+    else:
+        print("Could not find any liked videos to analyze in that period")
+
+
 
     print("\nGmail:")
     for m in client.gmail.list_inbox():
         print(m)
+
+    
 
 if __name__ == "__main__":
     main()
