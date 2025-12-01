acdbbe69eef2fa8b1c5f1ae8637fa38671372924
Matthew <matthewcflam@gmail.com>
2025-11-30 19:01:36 -0800

    Change Background

--- frontend/analytics_board/src/App.jsx
+++ frontend/analytics_board/src/App.jsx
@@ -20,7 +20,8 @@ import GoogleDashboardPages from "./components/GoogleDashboardPages";
 import InstagramDashboardPages from "./components/InstagramDashboardPages";
 import CardSwap from "./components/CardSwap"
 import UploadBox from "./components/UploadBox";
-import LiquidEther from "./components/LiquidEther";
+import DarkVeil from "./components/DarkVeil";
+
 
 
 export default function App() {
@@ -99,25 +100,8 @@ export default function App() {
       {/* GLOBAL BACKGROUND - Only show in main mode */}
       {mode === "main" && (
         <div className="absolute inset-0 -z-10 bg-black">
-
-          <div style={{ width: '100%', height: 600, position: 'relative' }}>
-            <LiquidEther
-              colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-              mouseForce={20}
-              cursorSize={100}
-              isViscous={false}
-              viscous={30}
-              iterationsViscous={32}
-              iterationsPoisson={32}
-              resolution={0.5}
-              isBounce={false}
-              autoDemo={true}
-              autoSpeed={0.5}
-              autoIntensity={2.2}
-              takeoverDuration={0.25}
-              autoResumeDelay={3000}
-              autoRampDuration={0.6}
-            />
+          <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+            <DarkVeil speed={2.0}  hueshift={250}/>
           </div>
         </div>
       )}
